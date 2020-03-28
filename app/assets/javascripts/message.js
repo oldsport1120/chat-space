87f74c4250a303eefc5d4d90e2e609371d954822
oldsport1120 <h.ito19821120@gmail.com>
2020-03-28 17:33:50 +0900

    メッセージを取得したら画面がスクロール

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -71,11 +71,14 @@ $(function(){
       data: {id: last_message_id}
     })
     .done(function(messages) {
-      var insertHTML = '';
-      $.each(messages, function(i, message) {
-        insertHTML += buildHTML(message)
-      });
-      $('.messages').append(insertHTML);
+      if (messages.length !== 0){
+        var insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.messages').append(insertHTML);
+        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      }
     })
     .fail(function() {
       alert('error');
